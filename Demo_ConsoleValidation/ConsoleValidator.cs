b6494b3285e9be7fcf172deef3540ea6f7afd551
John Velis <john@johnvelis.com>
2017-09-01 10:32:05 -0400

    1031

--- Demo_ConsoleValidation/ConsoleValidator.cs
+++ Demo_ConsoleValidation/ConsoleValidator.cs
@@ -9,7 +9,7 @@ namespace Demo_ConsoleValidation
     public static class ConsoleValidator
     {
         /// <summary>
-        /// helper method to get a valid integer from the user
+        /// helper method to get a valid integer from the user within a range
         /// </summary>
         /// <param name="minValue">inclusive minimum value</param>
         /// <param name="maxValue">inclusive maximum value</param>
@@ -99,6 +99,266 @@ namespace Demo_ConsoleValidation
             }
 
             return userInteger;
+        }
+
+        /// <summary>
+        /// helper method to get a valid integer from the user with a minimum value
+        /// </summary>
+        /// <param name="minValue">inclusive minimum value</param>
+        /// <param name="maxAttempts">maximum number of attempts</param>
+        /// <param name="pluralName">plural name of item</param>
+        /// <param name="validInput">indicates valid user input</param>
+        /// <returns></returns>
+        public static int GetIntegerFromUser(int minValue, int maxAttempts, string pluralName, out bool maxAttemptsExceeded)
+        {
+            bool validInput = false;
+            maxAttemptsExceeded = false;
+            string userResponse;
+            string feedbackMessage = "";
+            int userInteger = 0;
+            int attempts = 1;
+
+            while (!validInput && !maxAttemptsExceeded)
+            {
+                //
+                // more attempts available
+                //
+                if (attempts <= maxAttempts)
+                {
+                    Console.Write($"Enter the number of {pluralName} (Must be at least {minValue}.):");
+
+                    userResponse = Console.ReadLine();
+                    Console.WriteLine();
+
+                    //
+                    // input is an Integer
+                    //
+                    if (int.TryParse(userResponse, out userInteger))
+                    {
+                        //
+                        // input is in range
+                        //
+                        if (userInteger >= minValue)
+                        {
+                            validInput = true;
+                        }
+                        //
+                        // input is not in range
+                        //
+                        else if (true)
+                        {
+                            feedbackMessage = $"The number {userInteger} is not at least {minValue}.";
+                        }
+                    }
+                    //
+                    // input is not an Integer
+                    //
+                    else
+                    {
+                        feedbackMessage = $"{userResponse} is not an integer.";
+                    }
+
+                    if (!validInput && attempts <= maxAttempts)
+                    {
+                        Console.WriteLine($"You entered: {userResponse}");
+                        Console.WriteLine(feedbackMessage);
+
+                        if (attempts < maxAttempts)
+                        {
+                            Console.WriteLine($"Please enter an integer greater than {minValue}.");
+                            Console.WriteLine("Press any key to try again.");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine("It appears you have exceeded the maximum number of attempts allowed.");
+                            Console.WriteLine("Press any key to continue.");
+                            Console.ReadKey();
+                        }
+
+                        Console.Clear();
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                    }
+
+                    attempts++;
+                }
+                else
+                {
+                    maxAttemptsExceeded = true;
+                }
+            }
+
+            return userInteger;
+        }
+
+        /// <summary>
+        /// helper method to get a valid positive integer from the user
+        /// </summary>
+        /// <param name="maxAttempts">maximum number of attempts</param>
+        /// <param name="pluralName">plural name of item</param>
+        /// <param name="validInput">indicates valid user input</param>
+        /// <returns></returns>
+        public static int GetIntegerFromUser(int maxAttempts, string pluralName, out bool maxAttemptsExceeded)
+        {
+            bool validInput = false;
+            maxAttemptsExceeded = false;
+            string userResponse;
+            string feedbackMessage = "";
+            int userInteger = 0;
+            int attempts = 1;
+
+            while (!validInput && !maxAttemptsExceeded)
+            {
+                //
+                // more attempts available
+                //
+                if (attempts <= maxAttempts)
+                {
+                    Console.Write($"Enter the number of {pluralName}:");
+
+                    userResponse = Console.ReadLine();
+                    Console.WriteLine();
+
+                    //
+                    // input is an Integer
+                    //
+                    if (int.TryParse(userResponse, out userInteger))
+                    {
+                        //
+                        // input is in range
+                        //
+                        if (userInteger > 0)
+                        {
+                            validInput = true;
+                        }
+                        //
+                        // input is not in range
+                        //
+                        else if (true)
+                        {
+                            feedbackMessage = $"The number {userInteger} is not a positive integer.";
+                        }
+                    }
+                    //
+                    // input is not an Integer
+                    //
+                    else
+                    {
+                        feedbackMessage = $"{userResponse} is not an integer.";
+                    }
+
+                    if (!validInput && attempts <= maxAttempts)
+                    {
+                        Console.WriteLine($"You entered: {userResponse}");
+                        Console.WriteLine(feedbackMessage);
+
+                        if (attempts < maxAttempts)
+                        {
+                            Console.WriteLine($"Please enter a positive integer.");
+                            Console.WriteLine("Press any key to try again.");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine("It appears you have exceeded the maximum number of attempts allowed.");
+                            Console.WriteLine("Press any key to continue.");
+                            Console.ReadKey();
+                        }
+
+                        Console.Clear();
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                    }
+
+                    attempts++;
+                }
+                else
+                {
+                    maxAttemptsExceeded = true;
+                }
+            }
+
+            return userInteger;
+        }
+
+        /// <summary>
+        /// helper method to get a valid positive integer from the user
+        /// </summary>
+        /// <param name="maxAttempts">maximum number of attempts</param>
+        /// <param name="userPrompt">user prompt</param>
+        /// <param name="validInput">indicates valid user input</param>
+        /// <returns></returns>
+        public static string GetYesNoFromUser(int maxAttempts, string userPrompt, out bool maxAttemptsExceeded)
+        {
+            bool validInput = false;
+            maxAttemptsExceeded = false;
+            string userResponse = "";
+            string feedbackMessage = "";
+            int attempts = 1;
+
+            while (!validInput && !maxAttemptsExceeded)
+            {
+                Console.Write($"{userPrompt} [Yes / No] ");
+                userResponse = Console.ReadLine();
+                Console.WriteLine();
+
+                //
+                // input is valid
+                //
+                if (userResponse.ToUpper() == "YES" || userResponse.ToUpper() == "NO")
+                {
+                    validInput = true;
+                }
+                //
+                // input is not valid
+                //
+                else if (true)
+                {
+                    feedbackMessage = $"\"{userResponse}\" is not a valid response.";
+                }
+
+
+                if (!validInput && attempts <= maxAttempts)
+                {
+                    Console.WriteLine($"You entered: {userResponse}");
+                    Console.WriteLine(feedbackMessage);
+
+                    if (attempts < maxAttempts)
+                    {
+                        Console.WriteLine($"\"{userResponse}\" is not a valid response.");
+                        Console.WriteLine($"Please enter either \"Yes\" or \"No\".");
+                        Console.WriteLine("Press any key to try again.");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.WriteLine("It appears you have exceeded the maximum number of attempts allowed.");
+                        Console.WriteLine("Press any key to continue.");
+                        Console.ReadKey();
+                    }
+
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+
+                attempts++;
+
+                if (attempts > maxAttempts)
+                {
+                    maxAttemptsExceeded = true;
+                }
+
+            }
+
+            return userResponse;
         }
     }
 }
--- Demo_ConsoleValidation/Program.cs
+++ Demo_ConsoleValidation/Program.cs
@@ -8,9 +8,37 @@ namespace Demo_ConsoleValidation
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            TestIntegerMethod();
+            //TestIntegerMethod();
+
+            TestYesNoMethod();
+        }
+
+        private static void TestYesNoMethod()
+        {
+            bool maxAttemptsExceeded;
+            string userResponse;
+
+            //
+            // (number of attempts allowed, text prompt, return number of attempt status)
+            //
+            userResponse = ConsoleValidator.GetYesNoFromUser(3, "Do you want to be a Wizard?", out maxAttemptsExceeded);
+
+            //
+            // echo status to user
+            //
+            if (!maxAttemptsExceeded)
+            {
+                Console.WriteLine($"You have entered {userResponse}.");
+            }
+            else
+            {
+                Console.WriteLine("Your are obviously a mere mortal and will be excommunicated from The Order!");
+            }
+
+            Console.ReadKey();
         }
 
         static void TestIntegerMethod()
@@ -21,7 +49,17 @@ namespace Demo_ConsoleValidation
             //
             // (minimum value, maximum value, number of attempts allowed, text prompt, return number of attempt status)
             //
-            userInteger = ConsoleValidator.GetIntegerFromUser(1, 10, 3, "cats", out maxAttemptsExceeded);
+            //userInteger = ConsoleValidator.GetIntegerFromUser(1, 10, 3, "cats", out maxAttemptsExceeded);
+
+            //
+            // (minimum value, number of attempts allowed, text prompt, return number of attempt status)
+            //
+            //userInteger = ConsoleValidator.GetIntegerFromUser(1, 3, "cats", out maxAttemptsExceeded);
+
+            //
+            // (minimum value, number of attempts allowed, text prompt, return number of attempt status)
+            //
+            userInteger = ConsoleValidator.GetIntegerFromUser(3, "cats", out maxAttemptsExceeded);
 
             //
             // echo status to user
@@ -32,7 +70,7 @@ namespace Demo_ConsoleValidation
             }
             else
             {
-                Console.WriteLine("Your are obviously a mere mortal and will be excomunicated from The Order!");
+                Console.WriteLine("Your are obviously a mere mortal and will be excommunicated from The Order!");
             }
 
             Console.ReadKey();
